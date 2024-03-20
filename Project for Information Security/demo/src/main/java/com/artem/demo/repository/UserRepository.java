197005c185c8b8729e86f30d965e37f040807436
Martin <115161917+Martin13025@users.noreply.github.com>
2024-03-20 18:22:54 -0300

    Update UserRepository.java

--- Project for Information Security/demo/src/main/java/com/artem/demo/repository/UserRepository.java
+++ Project for Information Security/demo/src/main/java/com/artem/demo/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.artem.demo.repository;
 
 import java.util.Optional;
-
 import com.artem.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
